4b34d9aef4ba150bb833210f29b6a518820a242d
baljash-cheema <baljash-cheema@northwestern.edu>
2021-10-19 14:49:56 -0500

    Update checkpoint2.sql

--- checkpoint2/src/checkpoint2.sql
+++ checkpoint2/src/checkpoint2.sql
@@ -1,7 +1,7 @@
 --Question 1:
 
 --D/A/M allegations and disciplined = 737
-SELECT DISTINCT(officer_id)
+SELECT COUNT (DISTINCT(officer_id))
 FROM data_officerallegation
 WHERE data_officerallegation.disciplined=True AND data_officerallegation.allegation_category_id IN
     (SELECT id
@@ -9,8 +9,10 @@ WHERE data_officerallegation.disciplined=True AND data_officerallegation.allegat
     WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical'
     OR data_allegationcategory.category_code IN ('08J', '024', '003', '003A', '003B', '003C', '003D', '003E'))
 
+UNION
+
 --D/A/M allegations and NOT disciplined = 880
-SELECT DISTINCT(officer_id)
+SELECT COUNT (DISTINCT(officer_id))
 FROM data_officerallegation
 WHERE data_officerallegation.disciplined=False AND data_officerallegation.allegation_category_id IN
     (SELECT id
@@ -18,8 +20,10 @@ WHERE data_officerallegation.disciplined=False AND data_officerallegation.allega
     WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical'
     OR data_allegationcategory.category_code IN ('08J', '024', '003', '003A', '003B', '003C', '003D', '003E'))
 
+UNION
+
 --Allegations but NOT in D/A/M and disciplined = 9601
-SELECT DISTINCT(officer_id)
+SELECT COUNT (DISTINCT(officer_id))
 FROM data_officerallegation
 WHERE data_officerallegation.disciplined=True AND data_officerallegation.allegation_category_id NOT IN
     (SELECT id
@@ -27,14 +31,17 @@ WHERE data_officerallegation.disciplined=True AND data_officerallegation.allegat
     WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical'
     OR data_allegationcategory.category_code IN ('08J', '024', '003', '003A', '003B', '003C', '003D', '003E'))
 
+UNION
+
 --Allegations but NOT in D/A/M and NOT disciplined = 21797
-SELECT DISTINCT(officer_id)
+SELECT COUNT (DISTINCT(officer_id))
 FROM data_officerallegation
 WHERE data_officerallegation.disciplined=False AND data_officerallegation.allegation_category_id NOT IN
     (SELECT id
     FROM data_allegationcategory
     WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical'
     OR data_allegationcategory.category_code IN ('08J', '024', '003', '003A', '003B', '003C', '003D', '003E'))
+
 
 --Question 2:
 -- All info on allegations with d/a/m = 1930
